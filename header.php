5ce67799a060399e8d2216634c4d43cc4753cfa0
Arnaud Drain <drain.arnaud@hotmail.fr>
2022-01-30 16:39:31 +0100

    add meta description

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Loisirs-Montagne, Lyon, l'association." />
     <?php wp_head(); ?>
 </head>
 
